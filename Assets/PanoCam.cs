a2f6d734781f26c30f2face5b194c914975bd1d7
Owen-Benner <owen.p.benner@gmail.com>
2021-03-09 12:20:29 -0600

    re-imported

---
+++ Assets/PanoCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PanoCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+			File.WriteAllBytes("Pano.PNG", I360Render.Capture(4096, false,
+				null, false));
+    }
+}
